f0bfddc775a1e15e68394fa29c1425c1404f0eff
sanchit-8 <sanchit.goel0123@gmail.com>
2020-08-31 10:41:02 +0530

    configured scheduler

--- api/api/__init__.py
+++ api/api/__init__.py
@@ -1,24 +1,21 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 import requests
 
-
-def create_app():
-    
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(set_ticket_expired, trigger='interval', seconds=1*60*60)
-    scheduler.add_job(delete_expired_ticket, trigger='interval', seconds=24*60*60)
-    scheduler.start()
-    try:
-        return app
-    except:
-        scheduler.shutdown()
-
 def set_ticket_expired():
+    print("hi")
     URL = "http://localhost:8000/ticket/expire/"
     requests.put(url=URL)
+    print("expired worked")
     return
 
 def delete_expired_ticket():
     URL = "http://localhost:8000/ticket/deleteexpired/"
     requests.put(url=URL)
+    print("delete worked")
     return
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(set_ticket_expired, trigger='interval', seconds=5*60*60)
+scheduler.add_job(delete_expired_ticket, trigger='interval', seconds=24*60*60)
+scheduler.start()
+print("hello")
